f9aceaf2296026a2422d1a8dfa90e1a0e5727c6f
praveen-pujar <pujarpraveen9@gmail.com>
2020-09-21 09:43:18 +0530

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const path = require('path');
 
 
 app.use(express.json());
-
+app.use(express.static("../client/build"));
+  
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.get('/api/users', (req, res) => {
   console.log('api/users called!!!!')
@@ -46,10 +50,10 @@ app.post('/api/size', (req, res) => {
 
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   
   app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
